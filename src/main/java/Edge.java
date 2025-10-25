2d66527e502860c85ae25be4ccd6def85f2274b4
Hazardiusoms <nuska27052006@gmail.com>
2025-10-26 03:42:38 +0500

    init base structure

---
+++ src/main/java/Edge.java
@@ -0,0 +1,19 @@
+public class Edge implements Comparable<Edge> {
+    public int src, dest, weight;
+
+    public Edge(int src, int dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + src + " - " + dest + ", w=" + weight + ")";
+    }
+}
---
+++ src/main/java/ResultWriter.java
@@ -0,0 +1,2 @@
+public class ResultWriter {
+}
